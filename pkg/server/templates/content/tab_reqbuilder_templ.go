d7fc2584a1beb24a10e0739e758f78920ef62738
Manuel Sanchez Pinar <aomanu@gmail.com>
2024-11-02 18:15:13 +0100

    cURL is actually the name of the project, the cli is curl

--- pkg/server/templates/content/tab_reqbuilder_templ.go
+++ pkg/server/templates/content/tab_reqbuilder_templ.go
@@ -53,7 +53,7 @@ func ReqBuilderTab(txsSet vsl.TransactionSet) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"tabRequest\" class=\"tabcontent\"><p>Here you can generate a <a href=\"https://curl.se/\" target=\"_blank\">cURL</a> command based on parsed transactions VSL tags.</p><form id=\"headerForm\" hx-post=\"/reqbuilder/\" hx-target=\"#reqBuilderResults\" hx-swap=\"innerHTML settle:0.3s\" hx-include=\"[name=&#39;logs&#39;]\"><fieldset><legend>Transaction: </legend> <select id=\"transactionSelect\" name=\"transaction\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div id=\"tabRequest\" class=\"tabcontent\"><p>Here you can generate a <a href=\"https://curl.se/\" target=\"_blank\">curl</a> command based on parsed transactions VSL tags.</p><form id=\"headerForm\" hx-post=\"/reqbuilder/\" hx-target=\"#reqBuilderResults\" hx-swap=\"innerHTML settle:0.3s\" hx-include=\"[name=&#39;logs&#39;]\"><fieldset><legend>Transaction: </legend> <select id=\"transactionSelect\" name=\"transaction\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
